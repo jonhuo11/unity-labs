ed633fa2e4dec81929451ab1b39f6a2fdf1fbc1b
Jonathan Huo <jonhuo11@gmail.com>
2020-12-22 01:31:22 -0500

    added unfinished map editor

---
+++ Assets/Scripts/MapEditor/MapEditor.cs
@@ -0,0 +1,148 @@
+﻿/*
+ * 
+ * author: @jonhuo11
+ * 
+ * map editor gameobject for creating 2d maps with custom tilesets (WIP custom tileset functionality)
+ * uses MapEditorHelper.cs for ScriptableObject helper that provides functionality
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Grid))]
+public class MapEditor : MonoBehaviour
+{
+
+    public static MapEditor Instance { get; private set; }
+
+    // tiling
+    Tile activeTile;
+
+    // layers
+    List<Tilemap> layers = new List<Tilemap>();
+    Tilemap activeLayer;
+    int newLayerSortOrder = 0;
+
+    Grid grid;
+
+    /*
+     * 
+     * use unity native TilemapRenderer and Tilemap class for rendering the tileset    
+     * 
+     */
+
+    // ================================================= LAYERS =======================================================
+    void CreateLayer(string layerName)
+    {
+        if (transform.Find(layerName) == null)
+        {
+            // spawn a new layer
+            GameObject newLayer = new GameObject();
+            newLayer.transform.SetParent(transform);
+            newLayer.name = layerName;
+
+            newLayer.AddComponent<Tilemap>();
+            newLayer.AddComponent<TilemapRenderer>();
+            Tilemap l = newLayer.GetComponent<Tilemap>();
+
+            l.GetComponent<TilemapRenderer>().sortingOrder = newLayerSortOrder;
+            newLayerSortOrder++;
+
+            layers.Add(l);
+            activeLayer = l;
+
+            Debug.Log("successfully added new layer named " + layerName);
+        }
+        else
+        {
+            Debug.Log("existing layer with name " + layerName + " was found");
+        }
+    }
+
+    void CycleLayers()
+    {
+        activeLayer = layers[layers.IndexOf(activeLayer) + 1 >= layers.Count ? 0 : layers.IndexOf(activeLayer) + 1];
+        Debug.Log("cycled layers, new active layer is now " + activeLayer.name);
+    }
+
+    // ================================================= PAINTING TILES ================================================
+    Vector3Int GetGridPositionFromScreenPosition(Vector2 ss)
+    {
+        Vector3 ws = Camera.main.ScreenToWorldPoint(ss);
+        Vector3Int tileCoords = new Vector3Int(Mathf.FloorToInt(ws.x), Mathf.FloorToInt(ws.y), 0);
+        return grid.WorldToCell(tileCoords);
+    }
+
+    void PaintTile(Vector3Int coords, Tile t)
+    {
+        activeLayer.SetTile(coords, t);
+        Debug.Log("set tile at " + coords + " to tile " + t + " on layer " + activeLayer.name);
+    }
+
+    void Update()
+    {
+        // when the user clicks, paint a tile there
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3Int mouseTilePos = GetGridPositionFromScreenPosition(Input.mousePosition);
+            Debug.Log("mouse clicked at grid coords: " + mouseTilePos);
+
+            if (activeTile != null)
+            {
+                PaintTile(mouseTilePos, activeTile);
+            }
+            else
+            {
+                Debug.LogError("no active tile found");
+            }
+        }
+
+        // cycle between layers
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CycleLayers();
+        }
+
+        // create layer
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            CreateLayer(Random.Range(0f, 999999999f).ToString());
+        }
+    }
+
+    void Start()
+    {
+        grid = GetComponent<Grid>();
+
+        // spawn a new layer to start working with
+        CreateLayer("default");
+        // select a initially equipped tile
+        activeTile = MapEditorHelper.Instance.GetTiles()[0];
+        if (activeTile == null)
+            Debug.LogError("no tiles in registry available for equipping, check MapEditorHelper settings");
+
+        // print out save files
+        Debug.Log("Save files: " + MapEditorHelper.ListToString(MapEditorHelper.Instance.GetSaveFileNames()));
+    }
+
+    // enforce singleton
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning("found existing instance of MapEditor, deleting this instance's gameobject");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            Debug.Log("MapEditor loaded");
+        }
+    }
+
+}
---
+++ Assets/Scripts/MapEditor/MapEditorHelper.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * 
+ * author: @jonhuo11
+ * 
+ * 2d map editor with custom tile support (WIP custom tile functionality)
+ * Loads custom tiles for painting from persistent tile save folder
+ * this is the ScriptableObject helper singleton asset that provides filesystem IO operations for the map editor
+ * ensure only one of these objects exist in your project assets
+ * 
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "MapEditorHelper")]
+public sealed class MapEditorHelper : ScriptableObject
+{
+
+    public static MapEditorHelper Instance { get; private set; }
+
+    [SerializeField] string saveFolderName = "saves"; // storing map files
+
+    // tilesets
+    // TODO: implement loading of custom tilesets from a tilesets folder in persistentDataPath
+    // for now just have static hardcoded list of tiles
+    [SerializeField] string tilesetFolderName = "tilesets"; // storing custom tilesets for painting maps with
+    [SerializeField] List<Tile> tilesToLoad = new List<Tile>();
+    HashSet<Tile> tiles = new HashSet<Tile>();
+
+    void LoadTileset()
+    {
+        tiles.Clear();
+        foreach (Tile t in tilesToLoad)
+        {
+            if (!tiles.Add(t))
+            {
+                Debug.Log("identical tile already exists, skipping to next tile:" + t);
+            }
+        }
+        Debug.Log("successfully loaded " + tiles.Count + " tiles into tile registry");
+    }
+
+    // ======================================= MAPEDITOR HELPER FUNCTIONS ==========================================
+
+    public Tile[] GetTiles()
+    {
+        return tiles.ToArray();
+    }
+
+    public bool IsTileRecognized(Tile t)
+    {
+        return tiles.Contains(t);
+    }
+
+    public bool SaveMap()
+    {
+        return false;
+    }
+
+    public string GetSaveFolderLocation()
+    {
+        return Application.persistentDataPath + "/" + saveFolderName;
+    }
+
+
+    public FileInfo[] GetSaveFiles()
+    {
+        try
+        {
+            DirectoryInfo di = new DirectoryInfo(GetSaveFolderLocation());
+
+            if (!di.Exists)
+            {
+                Directory.CreateDirectory(GetSaveFolderLocation());
+                Debug.Log("did not find existing save folder, creating new one at " + GetSaveFolderLocation());
+            }
+            return di.GetFiles();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+            return null;
+        }
+    }
+
+    public string[] GetSaveFileNames()
+    {
+        FileInfo[] files = GetSaveFiles();
+        List<string> output = new List<string>();
+        foreach (FileInfo fi in files)
+        {
+        output.Add(fi.Name);
+        }
+        return output.ToArray();
+    }
+
+    // enforce singleton
+    void OnEnable()
+    {
+        MapEditorHelper[] h = Resources.FindObjectsOfTypeAll<MapEditorHelper>();
+        MapEditorHelper first = h.FirstOrDefault();
+        Debug.Log("MapEditorHelpers found: " + ListToString(h));
+        if (h.Length == 0)
+        {
+            Debug.LogError("found no MapEditorHelper asset, needs at least one in scene, using default");
+        }
+        else if (h.Length != 1)
+        {
+            Debug.LogError("found more than 1 MapEditorHelper asset in resources, there should only be one, will use first found:" + first);
+        }
+        Instance = first;
+
+        // load the tileset
+        LoadTileset();
+    }
+
+    public override string ToString()
+    {
+        return saveFolderName;
+    }
+
+    //  ================================== SOME STATIC HELPER FUNCTIONS =============================================
+
+    public static string ListToString<T>(T[] ts)
+    {
+        return "[" + string.Join(", ", (ts.Select(p => p.ToString()).ToArray())) + "]";
+    }
+
+}
